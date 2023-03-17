1959b09f6f857435d8009536a28d08f416ea38fa
tobe <tobeg3oogle@gmail.com>
2023-03-17 19:23:34 +0800

    Add notebook in docker image

---
+++ docker/local_run_docker_notebook.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -it -p 8888:8888 tobegit3hub/openmldb-chatgpt ./start_notebook.sh
---
+++ docker/start_notebook.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+jupyter notebook --allow-root --ip 0.0.0.0 --port 8888 --NotebookApp.token='' --NotebookApp.password=''
